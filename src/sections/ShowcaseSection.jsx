42785693d3c3d0052e52130952e5c6dcc7255f62
Tushar Tikkiwal <ttikkiwal@gmail.com>
2026-01-02 02:14:25 +0530

    Portfolio

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -3,18 +3,30 @@ const ShowcaseSection = () => {
     <div id="work" className="app-showcase">
       <div className="w-full">
         <div className="showcaselayout">
-          <div className="first-project-wrapper">
+          <div className="first-project-wrapper overflow-hidden">
             <div className="image-wrapper">
               <img src="/images/project.png.jpeg" alt="Ryde" />
             </div>
             <div className="text-content">
-              <h2>On-Demand Movie Application Made Simple with a Powerful, User-Friendly App called Renex</h2>
+              <h2>
+                On-Demand Movie Application Made Simple with a Powerful,
+                User-Friendly App called Renex
+              </h2>
               <p className="text-white-50 md:text-xl">
-                An app built with React Native, Expo, & TailwindCSS for a fast, user-friendly experience.
+                An app built with React Native, Expo, & TailwindCSS for a fast,
+                user-friendly experience.
               </p>
             </div>
           </div>
-          
+          <div className="project-list-wrapper overflow-hidden">
+            {/* error  */}
+            <div>
+              <img
+                src="/images/project2.png"
+                alt="Library Managment platform"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
