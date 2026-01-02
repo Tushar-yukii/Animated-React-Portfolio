0222a4b3bb1549dd7cfe6d32da0dacd3b8650c94
Tushar Tikkiwal <ttikkiwal@gmail.com>
2026-01-03 01:08:44 +0530

    Portfolio

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -5,12 +5,12 @@ const ShowcaseSection = () => {
         <div className="showcaselayout">
           <div className="first-project-wrapper overflow-hidden">
             <div className="image-wrapper">
-              <img src="/images/project.png.jpeg" alt="Ryde" />
+              <img src="/images/project.png.jpeg" alt="Renex" />
             </div>
             <div className="text-content">
               <h2>
                 On-Demand Movie Application Made Simple with a Powerful,
-                User-Friendly App called Renex
+                User-Friendly App called Renex.
               </h2>
               <p className="text-white-50 md:text-xl">
                 An app built with React Native, Expo, & TailwindCSS for a fast,
@@ -19,13 +19,17 @@ const ShowcaseSection = () => {
             </div>
           </div>
           <div className="project-list-wrapper overflow-hidden">
-            {/* error  */}
-            <div>
-              <img
-                src="/images/project2.png"
-                alt="Library Managment platform"
-              />
+            <div className="image-wrapper bg-[#ffefdb]">
+              <img src="/images/project2.2.png" alt="NewsApp Application" />
             </div>
+            <h2>News Application</h2>
+          </div>
+          {/* error */}
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="image-wrapper bg-[#ffe7eb]">
+              <img src="/images/project3.png" alt="" />
+            </div>
+            <h2>Blog App</h2>
           </div>
         </div>
       </div>
