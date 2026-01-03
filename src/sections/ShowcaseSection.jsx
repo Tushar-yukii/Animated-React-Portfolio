60fb09035eff73b57cc5671b28427d2772e5cddf
Tushar Tikkiwal <ttikkiwal@gmail.com>
2026-01-03 21:54:18 +0530

    Portfolio

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,16 +1,62 @@
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const ShowcaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+    const projects = [
+      project1Ref.current,
+      project2Ref.current,
+      project3Ref.current,
+    ];
+    projects.forEach((projectCard, index) => {
+      gsap.fromTo(
+        projectCard,
+        {
+          y: 50,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: projectCard,
+            start: "top bottom-=100",
+          },
+        }
+      );
+    });
+
+    gsap.fromTo(
+      sectionRef.current,
+      { opacity: 0 },
+      { opacity: 1 },
+      { duration: 1.5 }
+    );
+  }, []);
+
   return (
-    <div id="work" className="app-showcase">
+    <div id="work" ref={sectionRef} className="app-showcase">
       <div className="w-full">
         <div className="showcaselayout">
-          <div className="first-project-wrapper overflow-hidden">
+          <div className="first-project-wrapper" ref={project1Ref}>
             <div className="image-wrapper">
-              <img src="/images/project.png.jpeg" alt="Renex" />
+              <img src="/images/project.png.jpeg" alt="movie-App" />
             </div>
             <div className="text-content">
               <h2>
-                On-Demand Movie Application Made Simple with a Powerful,
-                User-Friendly App called Renex.
+                On-Demand Movie-app Simple with a Powerful, User-Friendly App
+                called Renex
               </h2>
               <p className="text-white-50 md:text-xl">
                 An app built with React Native, Expo, & TailwindCSS for a fast,
@@ -18,18 +64,25 @@ const ShowcaseSection = () => {
               </p>
             </div>
           </div>
-          <div className="project-list-wrapper overflow-hidden">
-            <div className="image-wrapper bg-[#ffefdb]">
-              <img src="/images/project2.2.png" alt="NewsApp Application" />
+
+          <div
+            className="project-list-wrapper overflow-hidden"
+            ref={project2Ref}
+          >
+            <div className="project">
+              <div className="image-wrapper bg-[#FFEFDB]">
+                <img src="/images/project2.2.png" alt="NewsApp" />
+              </div>
+              <h2>NewsApp Platform</h2>
             </div>
-            <h2>News Application</h2>
-          </div>
-          {/* error */}
-          <div className="project-list-wrapper overflow-hidden">
-            <div className="image-wrapper bg-[#ffe7eb]">
-              <img src="/images/project3.png" alt="" />
+
+            <div className="project" ref={project3Ref}>
+              {/* error in project3 */}
+              <div className="image-wrapper bg-[#FFE7EB]">
+                <img src="/images/project3.png" alt="YC Directory App" />
+              </div>
+              <h2>YC Directory - A Startup Showcase App</h2>
             </div>
-            <h2>Blog App</h2>
           </div>
         </div>
       </div>
