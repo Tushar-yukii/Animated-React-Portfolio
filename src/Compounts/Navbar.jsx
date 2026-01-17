33d5a07204feb4cdb349195125d7a89cb30a5e17
Tushar Tikkiwal <ttikkiwal@gmail.com>
2026-01-18 00:02:52 +0530

    Portfolio

--- src/Compounts/Navbar.jsx
+++ src/Compounts/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="navbar">
+      <div className="inner">
+        <a className="logo" href="#hero">
+          Tushar Tikkiwal
+          {/* error */}
+        </a>
+        <nav className="desktop">{/* error */}</nav>
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
